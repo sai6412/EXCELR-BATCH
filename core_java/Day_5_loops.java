5d8d6996713dcfdcece159376d265925dfa8e20c
sai6412 <g.sai641642@gmail.com>
2025-01-08 22:53:28 -0800

    Add files via upload

---
+++ core_java/Day_5_loops.java
@@ -0,0 +1,15 @@
+public class Day_5_loops {
+    public static void main(String[] args) {
+        String c ="classroom";
+        System.out.println(c.substring(c.length() -3));//print last 3 letters or char of string
+        System.out.println(c.substring(c.length() -1));//print last 1 letters or char of string
+        //loops
+        //for loop
+        //for(initialize;condition;incrementation)
+        for(int count=1;count<=10;count++){
+            System.out.println("sai");
+        }
+        
+    }
+    
+}
